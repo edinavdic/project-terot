1baa0883c05fb80b71bd49ac7e0260838121811c
Edin Avdic <eavdic2@etf.unsa.ba>
2018-07-25 03:22:40 +0200

    New activity and classes

---
+++ TerotGame/app/src/main/java/com/github/project_terot/terotfirstgame/models/Pantherai.java
@@ -0,0 +1,14 @@
+package com.github.project_terot.terotfirstgame.models;
+
+public abstract class Pantherai {
+    int strength, dexterity, intelligence;
+    // boja(preko nje weaknesses)
+    // slika ?
+    // mane/abilitiji
+
+    Pantherai(int strength, int dexterity, int intelligence){
+        this.strength = strength;
+        this.dexterity = dexterity;
+        this.intelligence = intelligence;
+    }
+}
